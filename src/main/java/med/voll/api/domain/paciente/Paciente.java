8e10b4684f112c619d33694961bbe56968b2d6f3
JotaPaiva <jonataspaiva2002@hotmail.com>
2023-08-18 11:15:58 -0300

    Implementando leitura, atualização e exclusão de pacientes.

--- src/main/java/med/voll/api/domain/paciente/Paciente.java
+++ src/main/java/med/voll/api/domain/paciente/Paciente.java
@@ -35,4 +35,21 @@ public class Paciente {
         this.ativo = true;
         this.endereco = new Endereco(dados.endereco());
     }
+
+    public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
+
+        if(dados.nome() != null) {
+            this.nome = dados.nome();
+        }
+        if(dados.telefone() != null) {
+            this.telefone = dados.telefone();
+        }
+        if(dados.endereco() != null) {
+            this.endereco.atualizarInformacoes(dados.endereco());
+        }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+    }
 }
